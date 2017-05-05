62de5bfc28f829147460c5491ab7f20a940d0682
lucascruz96 <lucascruz96@outlook.com>
2017-05-05 08:19:50 -0300

    Alterações no modelo

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
+	belongs_to :user
+	belongs_to :category
+	belongs_to :week
+	
 	validates :title, presence: true, length: {maximum: 50}
 
 	validates :description, presence: true, length: {maximum: 260}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_many :task
+	
 	validates :name, presence: true, length: {maximum: 50}
 
 	validates :password, presence: true, length: {maximum: 4}
